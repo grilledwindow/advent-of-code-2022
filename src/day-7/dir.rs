c8eef0baebab7b0e0ebd16a18c800b0d08426d21
xavier_ang <xaveang@gmail.com>
2022-12-08 22:38:17 +0800

    7: Rename children to dirs
    7: Rename children to dirs

--- src/day-7/dir.rs
+++ src/day-7/dir.rs
@@ -2,16 +2,12 @@
 pub(super) struct Dir<'a> {
     name: &'a str,
     size: u32,
-    children: Option<Vec<Dir<'a>>>,
+    dirs: Option<Vec<Dir<'a>>>,
 }
 
 impl<'a> Dir<'a> {
-    pub(super) fn new(name: &'a str, size: u32, children: Option<Vec<Dir<'a>>>) -> Self {
-        Self {
-            name,
-            size,
-            children,
-        }
+    pub(super) fn new(name: &'a str, size: u32, dirs: Option<Vec<Dir<'a>>>) -> Self {
+        Self { name, size, dirs }
     }
 
     pub(super) fn calculate_size(&mut self) -> u32 {
@@ -20,29 +16,29 @@ impl<'a> Dir<'a> {
         size
     }
 
-    pub(super) fn add_child_dir(&mut self, dir: Dir<'a>) {
-        if let Some(children) = &mut self.children {
-            children.push(dir);
+    pub(super) fn add_dir(&mut self, dir: Dir<'a>) {
+        if let Some(dirs) = &mut self.dirs {
+            dirs.push(dir);
         } else {
-            self.children = Some(vec![dir]);
+            self.dirs = Some(vec![dir]);
         }
     }
 
-    pub(super) fn set_children(&mut self, children: Option<Vec<Dir<'a>>>) {
-        self.children = children;
+    pub(super) fn set_dirs(&mut self, dirs: Option<Vec<Dir<'a>>>) {
+        self.dirs = dirs;
     }
 
     fn get_size(&self) -> u32 {
         self.size
     }
 
-    fn get_children(&self) -> &Option<Vec<Dir<'a>>> {
-        &self.children
+    fn get_dirs(&self) -> &Option<Vec<Dir<'a>>> {
+        &self.dirs
     }
 
     fn _get_size(&self, dir: &Dir) -> u32 {
-        if let Some(children) = dir.get_children() {
-            children
+        if let Some(dirs) = dir.get_dirs() {
+            dirs
                 .into_iter()
                 .fold(dir.size, |_size, _dir| _size + self._get_size(&_dir))
         } else {
