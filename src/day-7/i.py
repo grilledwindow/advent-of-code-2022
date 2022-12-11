168b4c4f5aa234a941867e2fbc99d1515400ff5f
xavier_ang <xaveang@gmail.com>
2022-12-12 02:17:17 +0800

    [py] 7: Fix a bunch of stuff
    Missing rstrip()'s, dir size not saving

--- src/day-7/i.py
+++ src/day-7/i.py
@@ -1,4 +1,7 @@
 from typing import Optional
+from enum import Enum
+
+DirType = Enum('DirType', ['DIR', 'FILE'])
 
 
 class Dir:
@@ -23,22 +26,21 @@ class Dir:
     def dirs(self) -> Optional[list['Dir']]:
         return self.__dirs
 
-    def set_dirs(self, dirs: Optional[list['Dir']]):
-        self.__dirs = dirs
+    def add_dir(self, dir: 'Dir'):
+        if self.__dirs is None:
+            self.__dirs = [dir]
+        else:
+            self.__dirs.append(dir)
 
     def __calc_size(self, dirs: Optional[list['Dir']]) -> int:
-        if dirs is None:
+        if self.__size is not None:
             return self.__size
-        elif self.__size is None:
-            self.__size = 0
-
-        return self.__size + sum(map(lambda dir: dir.size(), dirs))
+        return sum(map(lambda dir: dir.size(), dirs))
 
 
 def main():
     root = Dir(None, '/', None, [])
     current_dir = root
-    current_dir_dirs = []
 
     with open('i.in', 'r') as f:
         for line in f.readlines():
@@ -46,30 +48,30 @@ def main():
             first = split[0]
             match first:
                 case '$':
-                    if current_dir is not None:
-                        current_dir.set_dirs(current_dir_dirs)
                     command = split[1]
                     match command:
                         case 'ls':
-                            current_dir_dirs = []
+                            pass
                         case 'cd':
-                            target_dir = split[2]
+                            target_dir = split[2].rstrip()
                             match target_dir:
                                 case '/':
                                     pass
                                 case '..':
-                                    current_dir = current_dir.root()
+                                    if current_dir is not None:
+                                        current_dir = current_dir.root()
                                 case _:
                                     if current_dir is not None:
                                         current_dir = next(
                                             filter(lambda dir: dir.name() == target_dir, current_dir.dirs()), None)
+
                 case 'dir':
                     dir = split[1].rstrip()
-                    current_dir_dirs.append(Dir(current_dir, dir, None, []))
+                    current_dir.add_dir(Dir(current_dir, dir, None, []))
                 case size_str:
                     size = int(size_str)
-                    file = split[1]
-                    current_dir_dirs.append(Dir(current_dir, file, size, None))
+                    file = split[1].rstrip()
+                    current_dir.add_dir(Dir(current_dir, file, size, None))
 
     print(root.size())
 
